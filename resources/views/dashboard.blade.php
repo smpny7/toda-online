15387a478a196d2f7237c63e1e2c5e440a0342e7
smpny7 <symphonymode7@icloud.com>
2020-10-26 21:23:58 +0900

    [add] icon registration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,39 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('ホーム') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('次に見る') }}
+            </h3>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <x-dashboard />
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('受講中の講座一覧') }}
+            </h3>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-dashboard />
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('後で見る') }}
+            </h3>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-dashboard />
             </div>
         </div>
     </div>
