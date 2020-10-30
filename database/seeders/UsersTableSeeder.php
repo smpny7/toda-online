54dfe4db68b276fdbd53a684502dd9ed63d45c15
smpny7 <symphonymode7@icloud.com>
2020-10-31 05:20:04 +0900

    [add] create user seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'test',
+                'email' => 'test@example.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$.vBmYboSAsyah45afwG1qe0gbt59aEbmdVkUnmyWQrJYrSaGlHDOC',
+                'remember_token' => Str::random(10),
+            ]
+        ]);
+    }
+}
