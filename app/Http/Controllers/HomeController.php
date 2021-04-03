0b63cab21a2376dc685359804c0e0f42d4c7a9d2
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-04-04 05:29:51 +0900

    [add] home percent card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Video;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,8 +12,12 @@ use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class HomeController extends Controller
 {
+    /**
+     * @return View
+     */
     public function index(): View
     {
+        $attendance = User::query()->findOrFail(Auth::id())->attendances;
         $watch_next_videos = Video::orderBy('id', 'ASC')->take(3)->get();
         foreach ($watch_next_videos as $video) {
             if (isset($video->path)) {
@@ -23,7 +26,10 @@ class HomeController extends Controller
                 $video->thumbnail = Storage::disk('local')->url('thumbnail/' . $video->id . '.jpg');
             }
         }
-        return view('home.index')->with('watch_next_videos', $watch_next_videos);
+
+        return view('home.index')
+            ->with('attendance', $attendance)
+            ->with('watch_next_videos', $watch_next_videos);
     }
 
     public function search(Request $request): View
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,42 @@
 <x-app-layout>
-    ホーム
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto px-4 sm:px-8">
+            <h1 class="font-bold text-3xl text-gray-700 tracking-widest my-12">受講中の講座</h1>
+            <div class="grid grid-cols-3 gap-8">
+                @foreach(config('const.CLASS') as $class_key => $class)
+                    <div class="flex items-center flex-wrap max-w-md mb-6 px-10 bg-white shadow-xl rounded-2xl h-20">
+                        <div class="flex items-center justify-center -m-6 overflow-hidden bg-white rounded-full">
+                            <svg class="w-32 h-32 transform translate-x-1 translate-y-1" aria-hidden="true">
+                                <circle
+                                    class="text-gray-300"
+                                    stroke-width="10"
+                                    stroke="currentColor"
+                                    fill="transparent"
+                                    r="50"
+                                    cx="60"
+                                    cy="60"
+                                />
+                                <circle
+                                    class="text-themeColor"
+                                    stroke-width="10"
+                                    stroke-dasharray="314.159"
+                                    stroke-dashoffset="251.3272"
+                                    stroke-linecap="round"
+                                    stroke="currentColor"
+                                    transform="rotate(-90) translate(-120 0)"
+                                    fill="transparent"
+                                    r="50"
+                                    cx="60"
+                                    cy="60"
+                                />
+                            </svg>
+                            <span class="absolute text-2xl text-themeColor">80%</span>
+                        </div>
+                        <p class="ml-10 font-medium text-gray-600 sm:text-xl">{{ $class }}</p>
+                        <span class="ml-auto text-md font-medium {{ $attendance->$class_key ? 'text-themeColor' : 'text-gray-300' }} hidden sm:block">{{ $attendance->$class_key ? '受講中' : '未受講' }}</span>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </x-app-layout>
